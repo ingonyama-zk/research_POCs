58e22db9569cfed040d70b9bea98f1e911460dc7
krakhit <61501745+krakhit@users.noreply.github.com>
2024-11-24 10:22:49 +0200

    rust only code

--- cpp/include/poly_test.cuh
+++
@@ -1,6 +0,0 @@
-// include/poly_test.cuh
-#ifndef POLY_TEST_CUH
-#define POLY_TEST_CUH
-
-void basic_commit(); 
-#endif // POLY_TEST_CUH
--- go/example_module/test_package/test_package.go
+++
@@ -1,6 +0,0 @@
-package test_package
-
-// Add adds two integers and returns the result.
-func Add(a, b int) int {
-    return a + b
-}
